25de5849719888fad5bcf31c4274e34106dfc64f
Ramit Pandey <ramitpandey2002@gmail.com>
2025-11-09 00:52:25 +0530

    configure Tailwind with global styles and work on hero section layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
 function HomePage() {
   return (
     <>
-      <h1 className="text-2xl">Welcome to the YC Directory</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Vertual
+          Competitions.
+        </p>
+      </section>
     </>
   );
 }
